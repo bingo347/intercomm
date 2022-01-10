fd5dc69905fa71cbbd462a78c813e48475240668
Dmitry Belyaev <mail@d-belyaev.ru>
2022-01-10 22:33:28 +0300

    feat: use custom OnceCell in StaticTypeMap

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -8,5 +8,6 @@ mod once_cell;
 mod static_type_map;
 mod untyped_box;
 
+pub(crate) use once_cell::OnceCell;
 pub(crate) use static_type_map::StaticTypeMap;
 pub(crate) use untyped_box::UntypedBox;
--- src/common/static_type_map.rs
+++ src/common/static_type_map.rs
@@ -1,30 +1,29 @@
+use super::{OnceCell, UntypedBox};
 use std::{any::TypeId, collections::HashMap};
-use tokio::sync::{OnceCell, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 type Map<V> = HashMap<TypeId, V>;
 
-pub(crate) struct StaticTypeMap<V = super::UntypedBox> {
+pub(crate) struct StaticTypeMap<V = UntypedBox> {
     inner: OnceCell<RwLock<Map<V>>>,
 }
 
 impl<V> StaticTypeMap<V> {
     pub(crate) const fn new() -> Self {
         Self {
-            inner: OnceCell::const_new(),
+            inner: OnceCell::new(),
         }
     }
 
     pub(crate) async fn read(&self) -> RwLockReadGuard<'_, Map<V>> {
-        self.get_or_init().await.read().await
+        self.get_or_init().read().await
     }
 
     pub(crate) async fn write(&self) -> RwLockWriteGuard<'_, Map<V>> {
-        self.get_or_init().await.write().await
+        self.get_or_init().write().await
     }
 
-    async fn get_or_init(&self) -> &RwLock<Map<V>> {
-        self.inner
-            .get_or_init(|| async { RwLock::new(Map::new()) })
-            .await
+    fn get_or_init(&self) -> &RwLock<Map<V>> {
+        self.inner.get_or_init(|| RwLock::new(Map::new()))
     }
 }
