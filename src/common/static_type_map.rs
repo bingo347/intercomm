e30542405bd6f251d0b2276f34641e89ba17a6a3
Dmitry Belyaev <mail@d-belyaev.ru>
2022-01-11 20:48:02 +0300

    feat: support for schedule removes from synchronous code

--- src/common/static_type_map.rs
+++ src/common/static_type_map.rs
@@ -1,11 +1,23 @@
 use super::{OnceCell, UntypedBox};
-use std::{any::TypeId, collections::HashMap};
+use parking_lot::Mutex;
+use std::{
+    any::TypeId,
+    collections::HashMap,
+    mem,
+    sync::atomic::{AtomicBool, Ordering},
+};
 use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 type Map<V> = HashMap<TypeId, V>;
 
 pub(crate) struct StaticTypeMap<V = UntypedBox> {
-    inner: OnceCell<RwLock<Map<V>>>,
+    inner: OnceCell<StaticTypeMapInner<V>>,
+}
+
+struct StaticTypeMapInner<V> {
+    has_to_remove: AtomicBool,
+    to_remove: Mutex<Vec<TypeId>>,
+    map: RwLock<Map<V>>,
 }
 
 impl<V> StaticTypeMap<V> {
@@ -16,14 +28,34 @@ impl<V> StaticTypeMap<V> {
     }
 
     pub(crate) async fn read(&self) -> RwLockReadGuard<'_, Map<V>> {
-        self.get_or_init().read().await
+        self.get_or_init().map.read().await
     }
 
     pub(crate) async fn write(&self) -> RwLockWriteGuard<'_, Map<V>> {
-        self.get_or_init().write().await
+        let inner = self.get_or_init();
+        let mut map = inner.map.write().await;
+        if inner.has_to_remove.load(Ordering::Relaxed) {
+            let mut to_remove = inner.to_remove.lock();
+            inner.has_to_remove.store(false, Ordering::Release);
+            let to_remove = mem::replace(&mut *to_remove, Vec::new());
+            for id in to_remove.into_iter() {
+                map.remove(&id);
+            }
+        }
+        map
     }
 
-    fn get_or_init(&self) -> &RwLock<Map<V>> {
-        self.inner.get_or_init(|| RwLock::new(Map::new()))
+    pub(crate) fn remove_when_possible(&self, id: TypeId) {
+        let inner = self.get_or_init();
+        inner.to_remove.lock().push(id);
+        inner.has_to_remove.store(true, Ordering::Release);
+    }
+
+    fn get_or_init(&self) -> &StaticTypeMapInner<V> {
+        self.inner.get_or_init(|| StaticTypeMapInner {
+            has_to_remove: AtomicBool::new(false),
+            to_remove: Mutex::new(Vec::new()),
+            map: RwLock::new(Map::new()),
+        })
     }
 }
